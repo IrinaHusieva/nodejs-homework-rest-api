c5703c4ae1d74cf2f4abd948ce92ceaf884bc35a
Iryna Husieva <02guseva02@gmail.com>
2023-10-25 14:24:03 +0200

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ import express from "express";
 
 import authController from "../../controllers/auth-controller.js";
 
-import { isEmptyBody, authenticate } from "../../middlewares/index.js";
+import { isEmptyBody, authenticate, upload } from "../../middlewares/index.js";
 
 import { validateBody } from "../../decorators/index.js";
 
@@ -23,5 +23,7 @@ authRouter.get('/current', authenticate, authController.getCurrent);
 authRouter.post('/logout', authenticate, authController.logout);
 
 authRouter.patch('/', authenticate, userUpdateFavorite, authController.updateSubscription);
+
+authRouter.patch("/avatars", authenticate, upload.single("avatar"), authController.updateAvatarUser);
 
 export default authRouter;
